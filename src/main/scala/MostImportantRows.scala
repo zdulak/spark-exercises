27134f7daa1b812305681afef3933a8b9d7a9667
zdulak <60813197+zdulak@users.noreply.github.com>
2022-04-14 13:27:46 +0200

    Add MostImportantRows solution. Add show.txt for next task

---
+++ src/main/scala/MostImportantRows.scala
@@ -0,0 +1,32 @@
+object MostImportantRows extends App {
+  import org.apache.spark.sql.SparkSession
+
+  val spark = SparkSession
+    .builder()
+    .appName("FlatMapNumbers")
+    .master("local[*]")
+    .getOrCreate()
+
+  import spark.implicits._
+  import org.apache.spark.sql.functions._
+
+  val priorities = Seq(("MV1", 1), ("MV2", 2), ("VPV",3), ("Others", 4)).toDF("value", "rank")
+  val input = Seq(
+    (1, "MV1"),
+    (1, "MV2"),
+    (2, "VPV"),
+    (2, "Others"),
+    (3, "MV1"),
+    (3, "MV2"),
+    (3, "VPV"),
+    (3, "Others")).toDF("id", "value")
+
+  val solution = input
+    .join(priorities, "value")
+    .groupBy($"id")
+    .agg(min($"rank").as("rank"))
+    .join(priorities, "rank")
+    .drop($"rank")
+  solution.show()
+
+}
